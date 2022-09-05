b321ccd9ed0805d382e7d35d935d14db3c564e44
MegumiMirai <2261768355@qq.com>
2022-09-05 23:14:50 +0800

    解决橡皮筋之间的冲突

--- 骨架/导航/js/megumi.js
+++ 骨架/导航/js/megumi.js
@@ -105,12 +105,15 @@
     * */
       //手指一开始的位置
       var startX = 0
+      var startY = 0
       //元素一开始的位置
       var elementX = 0
       //手指距离触屏时的距离
       var disX = 0
       //var translateX = 0
       var index = 0
+      let isX = true
+      let isFirst = true
 
       //手指触碰屏幕时的函数
       carouselWrap.addEventListener('touchstart', function (ev){
@@ -135,19 +138,35 @@
           megumi.css(ulNode, 'translateX', -index * document.documentElement.clientWidth)
         }
 
-
-
         startX = TouchC.clientX
+        startY = TouchC.clientY
         elementX = megumi.css(ulNode, 'translateX')
+        isX = true
+        isFirst = true
       })
       // 手指滑动屏幕
       carouselWrap.addEventListener('touchmove', function (ev){
+        //如果初次滑动方向不是y轴，都不执行下面的逻辑
+        if(!isX){
+          return
+        }
         ev = ev || event
-        var TouchC = ev.changedTouches[0]
+        let TouchC = ev.changedTouches[0]
         //手指当前的位置
-        var nowX = TouchC.clientX
+        let nowX = TouchC.clientX
+        let nowY = TouchC.clientY
         //手指距离触屏时的距离
         disX = nowX - startX
+        disY = nowY - startY
+
+        //如果手指在y轴移动的距离大于x轴移动的距离，不触发轮播图的滑动事件
+        if(isFirst){
+          isFirst = false
+          if(Math.abs(disY) > Math.abs(disX)){
+            isX = false
+            return
+          }
+        }
 
         // translateX = elementX + disX
         // //将ul的left设置为原来的left+改变的距离
@@ -238,5 +257,117 @@
       }
 
     }
+  }
+  w.megumi.dragNav = function (){
+    //滑屏区域
+    let wrap = document.querySelector('.megumi-nav-drag-wrapper')
+    //滑屏元素
+    let item = document.querySelector('.megumi-nav-drag-wrapper .list')
+    //滑屏可以滑动的最大值
+    let minX = wrap.clientWidth - item.clientWidth
+    //滑屏元素一开始的位置 手指一开始的位置
+    let elementX
+    let startX
+    //实现快速滑屏的变量
+    //手指触摸的时候的时间以及item的位置
+    let lastTime = 0
+    let lastPoint = 0
+    //手指移动的时间差和距离差
+    let disTime = 0
+    let disPoint = 0
+    wrap.addEventListener('touchstart', function (ev){
+      //关闭橡皮筋效果
+      item.style.transition = 'none'
+      var touchC = ev.changedTouches[0]
+      startX = touchC.clientX
+      elementX = megumi.css(item, 'translateX')
+      //修改lastTime和lastPoint
+      lastTime = new Date().getTime()
+      lastPoint = touchC.clientX
+      // lastPoint = megumi.css(item, 'translateX')
+    })
+
+    wrap.addEventListener('touchmove', function (ev){
+      let touchC = ev.changedTouches[0]
+      let nowX = touchC.clientX
+      let disX = nowX - startX
+      let translateX = elementX + disX
+      //橡皮筋效果，随着向右拉动，item移动的距离逐渐变小
+      //在move的过程中，每一次手指touchmove真正的有效距离在慢慢变小，元素的滑动距离还是在变大
+      //disX：整个move过程的实际距离
+      //disPoint：整个手指touchmove的真正有效距离
+
+      //修改nowTime和nowPoint
+      let nowTime = new Date().getTime()
+      let nowPoint = touchC.clientX
+      // let nowPoint = megumi.css(item, 'translateX')
+      disTime = nowTime - lastTime
+      disPoint = nowPoint - lastPoint
+
+      lastTime = nowTime
+      lastPoint = nowPoint
+
+      if(translateX > 0){
+        item.handMove = true
+        // (0, 0.5)
+        let scale = document.documentElement.clientWidth / ((document.documentElement.clientWidth + translateX) * 2)
+        //使用这种方式会导致itemscale可能会变为负值，要保证scale是大于0的
+        // var scale = 1 - (disX / wrap.clientWidth)
+        translateX = megumi.css(item, 'translateX') + disPoint * scale
+        //不能用elementX，要获得item的实时距离
+        // translateX = elementX + disX * scale
+      }else if(translateX < minX){
+        item.handMove = true
+        let over = minX - translateX
+        let scale = document.documentElement.clientWidth / ((document.documentElement.clientWidth + over) * 2)
+        translateX = megumi.css(item, 'translateX') + disPoint * scale
+      }else{
+        item.handMove = false
+      }
+      // console.log(translateX)
+      megumi.css(item, 'translateX', translateX)
+    })
+
+    wrap.addEventListener('touchend', function (){
+      if(!item.handMove){
+        let translateX = megumi.css(item, 'translateX')
+        //计算手指滑动的速度
+        let speed = disPoint / disTime
+        //如果速度很小，让它不要有过渡效果，将speed变成0即可
+        speed = Math.abs(speed) < 0.2 ? 0 : speed
+        let targetX = translateX + speed * 200
+        //如果item的translateX大于零的话，让它重置为0
+        // item.style.transition = '0.5s transform'
+        let bsr = ""
+        let time = Math.abs(speed) * 0.2
+        time = time > 0 && time < 1 ? 1 : time
+        if(targetX > 0){
+          // item.style.transition = '0.5s transform'
+          targetX = 0
+          // megumi.css(item, 'translateX', translateX)
+          bsr = "cubic-bezier(.26, 1.51, .68, 1.54)"
+        }else if(targetX < minX){
+          // item.style.transition = '0.5s transform'
+          //如果item向左滑动的距离超过了他最远的距离，让它重置为最远的距离
+          targetX = minX
+          // megumi.css(item, 'translateX', translateX)
+          bsr = "cubic-bezier(.26, 1.51, .68, 1.54)"
+        }
+        item.style.transition = time + "s "+ bsr +" transform"
+        megumi.css(item, 'translateX', targetX)
+      }else{
+        let translateX = megumi.css(item, 'translateX')
+        //如果item的translateX大于零的话，让它重置为0
+        item.style.transition = '0.5s transform'
+        if(translateX > 0){
+          translateX = 0
+          megumi.css(item, 'translateX', translateX)
+        }else if(translateX < minX){
+          //如果item向左滑动的距离超过了他最远的距离，让它重置为最远的距离
+          translateX = minX
+          megumi.css(item, 'translateX', translateX)
+        }
+      }
+    })
   }
 })(window)
